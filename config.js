fff0b546c87cadc802cbff0ddc27bc7003cd9e5b
SequoiaUX <crackedzeropointgaming@gmail.com>
2025-04-10 12:15:22 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   users: {
     // You can add multiple users by doing username: 'password'.
     unrealprojectdev: "devedition2025",
-    luccaiscoollol: "truefriend25",
+    luccaisseamen: "horraylucca",
   },
 };
 
